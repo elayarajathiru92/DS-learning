9e042f21dd28c50e861ee84804474e8ea5cb531d
elayarajathiru92 <151925193+elayarajathiru92@users.noreply.github.com>
2023-12-20 03:23:54 +0000

    update

--- fast_api_sample.py
+++ fast_api_sample.py
@@ -2,17 +2,26 @@
 #pip install uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+city = pd.read_csv('/workspaces/DS-learning/Tetuan City power consumption.csv')
+x=city[["Temperature","Humidity","Wind Speed"]].values
+y=city["Zone 1 Power Consumption"].values
 
 app = FastAPI()
 
-class Add(BaseModel):
-    n1: int
-    n2: innt
+model=LinearRegression().fit(x,y)
 
-@app.post("/add")
-async def add(numbers: Add):
-    sum = numbers.n1 + numbers.n2
-    return {"result": sum}
+class predict(BaseModel):
+    temp: float
+    hum: float
+    wind: float
+
+@app.get("/predit_value")
+async def predit_value(pr: predict):
+    rel = model.predict([[pr.temp,pr.hum,pr.wind]])
+    return {"result": rel}
 
 if __name__ == "__main__":
     import uvicorn
